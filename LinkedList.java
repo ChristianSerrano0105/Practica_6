f39fdb0f137549b4200c40e5e1eb44a3a7abac18
Christian Serrano <cbi2243012797@izt.uam.mx>
2026-07-13 22:39:09 -0600

    Agregando el constructor de Nodo, asi como seclarando Nodo primero, ultimo, los cuales se van a null, y agregando el tamaño

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,33 @@
 import java.util.Iterator;
 
 public class LinkedList<E> implements Lista<E>{
-    private class Nodo <E> 
+    
+    private class Nodo <E> {
+        private Nodo <E> siguiente = null;
+        private E info= null;
+
+        Nodo (Nodo <E> siguiente, E info){
+            this.siguiente = siguiente;
+            this.info = info;
+        }
+        public Nodo<E> getSiguiente(){
+            return siguiente;
+        }
+
+        public void setSiguiente(Nodo<E> siguiente){
+            this.siguiente = siguiente;
+        }
+
+        public E getInfo(){
+            return info;
+        }
+
+        public void setInfo(E info){
+            this.info= info;
+        }
+    }
+
+    private Nodo<E> primero = null;
+    private Nodo<E> ultimo = null;
+    private Nodo<E> tamanio=0;
 }
